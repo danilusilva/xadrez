ff7eb97ba7c09f2bc0e4939cc7d14b7b90a2095d
Danilu Silva <dan7zada@gmail.com>
2026-06-25 14:58:39 -0300

    feat: criando a classe PosicaoXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,22 +1,26 @@
 ﻿using xadrez;
 using xadrez_console;
 using xadrez_console.tabuleiro;
+using xadrez_console.xadrez;
 
-try
-{
+//try
+//{
+//Tabuleiro tab = new Tabuleiro(8, 8);
 
-    Tabuleiro tab = new Tabuleiro(8, 8);
+//tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+//tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 7));
+//tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(7, 5));
 
-    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 7));
-    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(7, 5));
+//Tela.imprimirTabuleiro(tab);
+PosicaoXadrez pos = new PosicaoXadrez('a', 7);
+Console.WriteLine(pos);
 
-    Tela.imprimirTabuleiro(tab);
-}
-catch (TabuleiroException e)
-{
-    Console.WriteLine(e.Message);
-}
+
+//}
+//catch (TabuleiroException e)
+//{
+//    Console.WriteLine(e.Message);
+//}
 
 Console.ReadLine();
 
