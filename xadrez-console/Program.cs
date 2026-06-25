711df6cf83b8148240208fd6cee8e34014261e70
Danilu Silva <dan7zada@gmail.com>
2026-06-25 09:53:33 -0300

    feat: criando classe posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using xadrez_console.tabuleiro;
+
+Posicao P = new Posicao(3, 4);
+
+Console.WriteLine("Posição: " + P);
+
+Console.ReadLine();
---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,21 @@
+﻿namespace xadrez_console.tabuleiro
+{
+    public class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha
+                + ", "
+                + Coluna;
+        }
+    }
+}
